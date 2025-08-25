7623197fd35d2c61ec5b5c67d80aaf5b9df90421
feras-3 <ferasdarwish02@gmail.com>
2025-08-25 16:51:22 +0300

    set up

---
+++ SSB/SSB/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SSB project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SSB.settings')
+
+application = get_asgi_application()
