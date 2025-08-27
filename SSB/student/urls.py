03389c7d332f7ab3e2504309439db58af3f6d78c
0yousif <ysoofee1@gmail.com>
2025-08-27 14:29:41 +0300

    registration static page

--- SSB/student/urls.py
+++ SSB/student/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.dashboard, name="dashboard")
+    path('',views.dashboard, name="dashboard"),
+    path('registration',views.registration, name="student-registration")
 ]
--- SSB/student/views.py
+++ SSB/student/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def registration(request):
+    
+    return render(request, 'registration.html')
