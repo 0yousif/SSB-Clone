ce70095070a54de9f959812251832f6f9e783cf9
alyaalaali <alya.alaali05@gmail.com>
2025-08-30 18:48:14 +0300

    created tutor assigned sections page

--- SSB/faculty/urls.py
+++ SSB/faculty/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('students/', views.student_lookup, name='student_lookup'),
     path('students/<int:student_id>/', views.student_detail, name='student_detail'),
     path('attendance/', views.take_attendance, name='take_attendance'),
+    path('enrolledsections/', views.tutor_sections, name='tutor_sections'),
+
 
 ]
