b0bf59a61f740cd94a5282e5db4201a1e09647bb
0yousif <ysoofee1@gmail.com>
2025-09-01 11:27:46 +0300

    plan sections are dynamic

--- SSB/adminstrator/admin.py
+++ SSB/adminstrator/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Profile, Section, Course, Departments, Semester, Location, Time, Section_schedules,Student_registration
-
+from student.models import Student_plan
 admin.site.register(Profile)
 admin.site.register(Departments)
 admin.site.register(Section)
@@ -10,3 +10,4 @@ admin.site.register(Location)
 admin.site.register(Time)
 admin.site.register(Section_schedules)
 admin.site.register(Student_registration)
+admin.site.register(Student_plan)
