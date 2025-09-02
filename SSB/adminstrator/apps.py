e37baa2494a24db387d0b2038d92b2722f813f42
0yousif <ysoofee1@gmail.com>
2025-09-02 16:52:59 +0300

    rearrange everything

--- SSB/adminstrator/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminstratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adminstrator'
-
-    def ready(self):
-        import adminstrator.signals
--- SSB/student/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Student_plan
-
-class StudentPlanForm(ModelForm):
-    class Meta:
-        model = Student_plan
-        fields = ['name']
