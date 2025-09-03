dbc8df3e4ce0b85e1d673a0b3e16779651143831
feras-3 <ferasdarwish02@gmail.com>
2025-09-03 05:54:34 +0300

    fix

--- SSB/adminstrator/admin.py
+++ SSB/adminstrator/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Section, Course, Departments, Semester, Location, Time, Section_schedules,Student_registration
+from .models import Profile, Section, Course, Departments, Semester, Location, Time, Section_schedules,Student_registration, Configurations
 from student.models import Student_plan
 admin.site.register(Profile)
 admin.site.register(Departments)
@@ -11,3 +11,4 @@ admin.site.register(Time)
 admin.site.register(Section_schedules)
 admin.site.register(Student_registration)
 admin.site.register(Student_plan)
+admin.site.register(Configurations)
