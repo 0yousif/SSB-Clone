84f876ca3e2e33de99d8cbf10fc04e58b6a7063f
0yousif <ysoofee1@gmail.com>
2025-09-05 03:17:31 +0300

    fixing the addmitions url

--- SSB/settings.py
+++ SSB/settings.py
@@ -84,7 +84,12 @@ WSGI_APPLICATION = 'SSB.wsgi.application'
 
 if 'ON_HEROKU' in os.environ:
     DATABASES = {
-        "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
+        "default": dj_database_url.config(
+            env='DATABASE_URL',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
 else:
     DATABASES = {
